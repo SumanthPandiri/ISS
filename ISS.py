314821dd701d9fa29d185109ab4943d16c7ed8fd
Sumanth Pandiri <sumanthpandiri@135.201.21.198.tigernet.wifi.dyn.clemson.edu>
2024-02-24 16:12:50 -0500

    Initial Commit

---
+++ ISS.py
@@ -0,0 +1,32 @@
+import urllib3
+import json
+import datetime
+import time
+
+
+z = True
+
+
+start = time.time()
+# displaying date and time in a regular string format
+while z:
+    
+    if (time.time() > start + 9):
+        #collect data from url
+
+        http = urllib3.PoolManager()
+        req = http.request('GET', "http://api.open-notify.org/iss-now.json")
+
+        #parse the data into JSON format
+        x = req.data.decode("utf-8")
+        y = json.loads(x)
+
+        #convert the timestamp from UNIX to regular date and time
+        unix_time = y['timestamp'] 
+        date_time = datetime.datetime.fromtimestamp(unix_time)
+        print("Date & Time:" , date_time.strftime('%Y-%m-%d %H:%M:%S'))
+
+        #display the Latitude and Longitude
+        print ("Latitude:", y['iss_position']['latitude'], "Longitude:", y['iss_position']['longitude'])
+        start = time.time()
+
